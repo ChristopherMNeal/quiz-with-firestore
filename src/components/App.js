5f6d5144d5059eda80d2340a7da2cf56f29ec274
Christopher Neal <christopher.m.neal@gmail.com>
2022-02-23 15:43:02 -0800

    add signin routes
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import QuizList from "./QuizList";
 import Signin from "./Signin";
+import Signup from "./Signup";
 import NewQuizForm from "./NewQuizForm";
 import QuizDetails from "./QuizDetails";
 import { withFirestore, isLoaded } from "react-redux-firebase";
@@ -19,6 +20,7 @@ class App extends React.Component {
       //2 newQuizForm
       //3 Signin
       //4 EditForm
+      //5 Signup
     };
   }
 
@@ -52,6 +54,12 @@ class App extends React.Component {
     });
   };
 
+  handleSignUpClick = () => {
+    this.setState({
+      currentPage: 5,
+    });
+  };
+
   handleBackHome = () => {
     this.setState({
       currentPage: 0,
@@ -60,17 +68,28 @@ class App extends React.Component {
 
   render() {
     const auth = this.props.firebase.auth();
+    console.log("auth = " + auth);
     let pageShowing = null;
+    let signInPageShowing = null;
     const handleReturnHome = this.handleBackHome;
     const handleSigninOnPage = this.handleSignIn;
     const handleClick = this.handleCreateClick;
+    const handleSignUpClick = this.handleSignUpClick;
 
     if (this.state.currentPage === 0) {
       pageShowing = <QuizList onQuizClick={this.handleQuizClick} />;
     } else if (this.state.currentPage === 2) {
       pageShowing = <NewQuizForm onQuizCreate={this.handleBackHome} />;
     } else if (this.state.currentPage === 3) {
-      pageShowing = <Signin />;
+      pageShowing = (
+        <Signin
+          handleHome={handleReturnHome}
+          handleSignUpClick={handleSignUpClick}
+        />
+      );
+      signInPageShowing = pageShowing;
+    } else if (this.state.currentPage === 5) {
+      signInPageShowing = <Signup handleSign={handleSigninOnPage} />;
     } else if (this.state.currentPage === 4) {
       pageShowing = (
         <EditQuizForm
@@ -107,6 +126,7 @@ class App extends React.Component {
             handleHome={handleReturnHome}
             handleSign={handleSigninOnPage}
           />
+          { signInPageShowing }
           <h1>You must be signed in to access the queue.</h1>
         </React.Fragment>
       );
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,66 +1,61 @@
 import React from "react";
 import firebase from "firebase/app";
+import { withFirestore, isLoaded } from "react-redux-firebase";
 
-function Signin() {
-  function doSignUp(event) {
+function Signin(props) {
+  function doSignIn(event) {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
+      .signInWithEmailAndPassword(email, password)
       .then(function () {
-        console.log("successfully signed up!");
+        console.log("Successfully signed in!");
+        props.handleHome();
       })
       .catch(function (error) {
         console.log(error.message);
       });
   }
 
-  function doSignIn(event) {
-    event.preventDefault();
-    const email = event.target.signinEmail.value;
-    const password = event.target.signinPassword.value;
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-      console.log("Successfully signed in!");
-    }).catch(function(error) {
-      console.log(error.message);
-    });
+  function doSignOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        console.log("Successfully signed out!");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
   }
 
-  function doSignOut() {
-    firebase.auth().signOut().then(function() {
-      console.log("Successfully signed out!");
-    }).catch(function(error) {
-      console.log(error.message);
-    });
+  function handleSignUp() {
+    props.handleSignUpClick();
   }
-  
-  return (
+
+  const auth = props.firebase.auth();
+  console.log("auth = " + auth);
+
+  return !isLoaded(auth) ? (
+    <h1>Loading...</h1>
+  ) : isLoaded(auth) && auth.currentUser == null ? (
     <React.Fragment>
-      <h1>Sign up:</h1>
-      <form onSubmit={doSignUp}>
-        <input type="text" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="Password" />
-        <button type="submit">Sign up</button>
-      </form>
       <h1>Sign In</h1>
       <form onSubmit={doSignIn}>
-        <input
-          type='text'
-          name='signinEmail'
-          placeholder='email' />
-        <input
-          type='password'
-          name='signinPassword'
-          placeholder='Password' />
-        <button type='submit'>Sign in</button>
+        <input type="text" name="signinEmail" placeholder="email" />
+        <input type="password" name="signinPassword" placeholder="Password" />
+        <button type="submit">Sign in</button>
       </form>
+      <p onClick={handleSignUp}>You new here? Sign up!</p>
+    </React.Fragment>
+  ) : (
+    <React.Fragment>
       <h1>Sign Out</h1>
       <button onClick={doSignOut}>Sign out</button>
-
     </React.Fragment>
   );
 }
 
-export default Signin;
+export default withFirestore(Signin);
